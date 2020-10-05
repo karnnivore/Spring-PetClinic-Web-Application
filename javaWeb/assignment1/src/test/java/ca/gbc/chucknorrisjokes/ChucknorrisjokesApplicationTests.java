665f37471fcccec0dc378862e22159758a86439a
Nick Chinsen <nicholaschinsen@gmail.com>
2020-10-05 13:58:04 -0400

    initial commit for assignment 1 Java Web Development

---
+++ javaWeb/assignment1/src/test/java/ca/gbc/chucknorrisjokes/ChucknorrisjokesApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.chucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChucknorrisjokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
