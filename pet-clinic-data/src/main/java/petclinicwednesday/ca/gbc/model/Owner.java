b3511db743a2d4fb83552669627747f4ad68a31f
Nick Chinsen <nicholaschinsen@gmail.com>
2020-11-24 21:59:40 -0500

    Added contact information properties to owner

--- pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/model/Owner.java
+++ pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/model/Owner.java
@@ -2,4 +2,7 @@ package petclinicwednesday.ca.gbc.model;
 
 public class Owner  extends Person {
     private Pet pet;
+    private String address;
+    private String city;
+    private String telephone;
 }
