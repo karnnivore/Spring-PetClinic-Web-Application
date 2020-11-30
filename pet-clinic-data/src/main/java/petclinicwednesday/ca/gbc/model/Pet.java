5ab91095328b5f8a84e338044f9c27dd025d2707
Nick Chinsen <nicholaschinsen@gmail.com>
2020-11-30 16:05:46 -0500

    Created Visit Entity

--- pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/model/Pet.java
+++ pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/model/Pet.java
@@ -1,10 +1,9 @@
 package petclinicwednesday.ca.gbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pet extends BaseEntity {
@@ -22,6 +21,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
