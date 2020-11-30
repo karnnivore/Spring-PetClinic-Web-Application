e23aec322b271b087bcccd3e557f9e8c62d43d77
Nick Chinsen <nicholaschinsen@gmail.com>
2020-11-30 16:30:49 -0500

    Created Spring Data JPA Service for Owners Closes #57

--- pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/repositories/SpecialtyRepository.java
+++ pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/repositories/SpecialtyRepository.java
@@ -1,6 +1,7 @@
 package petclinicwednesday.ca.gbc.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import petclinicwednesday.ca.gbc.model.Specialty;
 
-public interface SpecialtyRepository extends CrudRepository {
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
 }
--- pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/repositories/VetRepository.java
@@ -1,6 +1,7 @@
 package petclinicwednesday.ca.gbc.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import petclinicwednesday.ca.gbc.model.Vet;
 
-public interface VetRepository extends CrudRepository {
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
