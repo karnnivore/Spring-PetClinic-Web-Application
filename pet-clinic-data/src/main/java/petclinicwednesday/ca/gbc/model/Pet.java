efacf9bc614ea43e4516942b0653722cb1c2c795
Nick Chinsen <nicholaschinsen@gmail.com>
2020-11-30 15:57:33 -0500

    Converted Owners to JPA Entities Closes #54

--- pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/model/Pet.java
+++ pet-clinic-data/src/main/java/petclinicwednesday/ca/gbc/model/Pet.java
@@ -1,11 +1,26 @@
 package petclinicwednesday.ca.gbc.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
+@Entity
 public class Pet extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthdate;
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
